06dfdb049b089c53da6d1264508c65ea788adb5d
MolochkovSA <wwwsergwww@mail.ru>
2024-08-29 21:36:54 +0300

    refact: FSD

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,11 @@
 import { Suspense } from 'react'
 import { Link, Route, Routes } from 'react-router-dom'
-import { AboutPageAsync } from './pages/AboutPage/AboutPage.async'
-import { MainPageAsync } from './pages/MainPage/MainPage.async'
-import { useTheme } from './theme/useTheme'
+import { AboutPage } from 'pages/AboutPage'
+import { MainPage } from 'pages/MainPage'
+import { classNames } from 'shared/lib'
+import { useTheme } from './providers/ThemeProvider'
 
 import './styles/index.scss'
-import { classNames } from './helpers/classNames/classNames'
 
 export const App = () => {
   const { theme, toggleTheme } = useTheme()
@@ -18,8 +18,8 @@ export const App = () => {
 
       <Suspense fallback={<div>Идет загрузка...</div>}>
         <Routes>
-          <Route path="/about" element={<AboutPageAsync />} />
-          <Route path="/" element={<MainPageAsync />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/" element={<MainPage />} />
         </Routes>
       </Suspense>
     </div>
--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -1,7 +1,13 @@
 import { FC, ReactNode, useMemo, useState } from 'react'
-import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext, ThemeContextProps } from './ThemeContext'
+import {
+  LOCAL_STORAGE_THEME_KEY,
+  Theme,
+  ThemeContext,
+  ThemeContextProps,
+} from '../lib/ThemeContext'
 
-const defaultTheme: Theme = (localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme) || Theme.LIGHT
+const defaultTheme: Theme =
+  (localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme) || Theme.LIGHT
 
 type ThemeProviderProps = {
   children: ReactNode
